adb40791aaec35a532db1610a44455b4829700be
Stix <125987377+udpstorm@users.noreply.github.com>
2024-08-25 14:13:10 +0200

    .

--- assets/js/player.js
+++ assets/js/player.js
@@ -4,7 +4,6 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const footer = document.getElementById('footer');
 
-// Array of songs with title and path
 const tracks = [
     { title: "Yeat - Already Rich", path: "assets/music/AlreadyRich.mp3" },
     { title: "Destroy Lonely - if looks could kill", path: "assets/music/iflookscouldkill.mp3" },
@@ -22,7 +21,6 @@ const tracks = [
 let currentTrack = 0;
 audioPlayer.volume = 0.15;
 
-// Shuffle tracks array
 function shuffleTracks() {
     for (let i = tracks.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -30,20 +28,17 @@ function shuffleTracks() {
     }
 }
 
-// Load a track and update the footer
 function loadTrack(index) {
     currentTrack = index;
     audioPlayer.src = tracks[currentTrack].path;
     footer.textContent = tracks[currentTrack].title;
 }
 
-// Load a random track on page load
 function loadRandomTrack() {
     shuffleTracks();
-    loadTrack(0); // Start with the first track in the shuffled list
+    loadTrack(0);
 }
 
-// Play/Pause functionality
 playPauseBtn.addEventListener('click', () => {
     if (audioPlayer.paused) {
         audioPlayer.play();
@@ -54,7 +49,6 @@ playPauseBtn.addEventListener('click', () => {
     }
 });
 
-// Play the next track
 function playNextTrack() {
     const nextTrack = (currentTrack + 1) % tracks.length;
     loadTrack(nextTrack);
@@ -62,10 +56,8 @@ function playNextTrack() {
     playPauseBtn.innerHTML = '<i class="icon fa-solid fa-pause"></i>';
 }
 
-// Next track button functionality
 nextBtn.addEventListener('click', playNextTrack);
 
-// Previous track functionality
 prevBtn.addEventListener('click', () => {
     const prevTrack = (currentTrack - 1 + tracks.length) % tracks.length;
     loadTrack(prevTrack);
@@ -73,8 +65,6 @@ prevBtn.addEventListener('click', () => {
     playPauseBtn.innerHTML = '<i class="icon fa-solid fa-pause"></i>';
 });
 
-// Play next track automatically when the current one ends
 audioPlayer.addEventListener('ended', playNextTrack);
 
-// Load a random track on page load
 loadRandomTrack();
